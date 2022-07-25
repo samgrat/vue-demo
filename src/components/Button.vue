97662f39d0b28d55dbcb4fcd250aee0d1a68d54d
Samuel Bamba <smail.bamba@gmail.com>
2022-07-25 21:30:12 +0200

    [0.4.0] Add Button, mod Header

---
+++ src/components/Button.vue
@@ -0,0 +1,10 @@
+<template>
+    <button :style="{ background : color }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: { color: String, text: String }
+    }
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,15 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
+        <Button color="green" text="Add Task" />
     </header>
 </template>
 
 <script>
+import Button from './Button.vue'
+
 export default {
+        components: { Button },
         name: 'Header',
         props: { title: String }
     }
