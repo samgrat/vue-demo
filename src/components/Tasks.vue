0190bb67549cdeb7d35288131ebbbd6a5679046a
Samuel Bamba <smail.bamba@gmail.com>
2022-07-26 15:21:17 +0200

    [0.6.0] Add Tasks, Task, mod HomeView

---
+++ src/components/Tasks.vue
@@ -0,0 +1,15 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <Task :task="task" />
+    </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+    components: { Task },
+    name: 'Tasks',
+    props: { tasks: Array }
+}
+</script>
